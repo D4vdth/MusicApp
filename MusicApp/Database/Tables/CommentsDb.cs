715948e420c26935353fd6299f9212dd48cb0009
Pipe <prietofelipe51@gmail.com>
2025-05-21 10:53:59 -0500

    Adding comments functionality

---
+++ MusicApp/Database/Tables/CommentsDb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using MusicApp.Models;
+using MySql.Data.MySqlClient;
+
+namespace MusicApp.Database.Tables
+{
+        public class CommentsDb
+        {
+            private static List<Comments> comments = new List<Comments>();
+
+            public List<Comments> GetBySongId(int songId)
+            {
+                return comments.Where(c => c.songId == songId).OrderByDescending(c => c.Timestamp).ToList();
+            }
+
+            public void Add(Comments comment)
+            {
+                comment.Timestamp = DateTime.Now;
+                comments.Add(comment);
+            }
+        }
+}
--- MusicApp/Database/database.sql
+++ MusicApp/Database/database.sql
@@ -25,6 +25,15 @@ CREATE TABLE Song (
     FOREIGN KEY (ArtistId) REFERENCES Artist(Id)
         ON DELETE CASCADE
 ) ENGINE=InnoDB;
+
+CREATE TABLE Comments (
+    id INT IDENTITY (1,1) PRIMARY KEY,
+    username VARCHAR(50),
+    comment VARCHAR(500),
+    commentDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (songId) REFERENCES Song(Id)
+        ON DELETE CASCADE
+);
    
 INSERT INTO Artist (Name) VALUES ('Luis Alfonso');
 INSERT INTO Artist (Name) VALUES ('Grupo Frontera');
@@ -37,15 +46,3 @@ INSERT INTO Album (Name, ArtistId, ReleaseDate) VALUES ('El Comienzo', (SELECT I
 INSERT INTO Album (Name, ArtistId, ReleaseDate) VALUES ('Suavemente', (SELECT Id FROM Artist WHERE Name = 'Elvis Crespo'), '1998-01-01'); 
 INSERT INTO Album (Name, ArtistId, ReleaseDate) VALUES ('Use Your Illusion I', (SELECT Id FROM Artist WHERE Name = 'Guns N''Roses'), '1991-01-01'); 
 INSERT INTO Album (Name, ArtistId, ReleaseDate) VALUES ('MANIFESTTING 20-05', (SELECT Id FROM Artist WHERE Name = 'Feid'), '2024-01-01');
-
-INSERT INTO Song (Title, ArtistId, Duration, AlbumId, FilePath, AlbumCoverPath)
-VALUES ('Cu�nto Vale', (SELECT Id FROM Artist WHERE Name = 'Luis Alfonso'), '00:03:37', (SELECT Id FROM Album WHERE Name = 'La Terraza'), '/views/Assets/Music/CuantoVale-LuisAlfonso/cuanto-vale.mp3', '/MusicApp;/views/Assets/Music/CuantoVale-LuisAlfonso/cuanto-vale.jpeg'); 
-INSERT INTO Song (Title, ArtistId, Duration, AlbumId, FilePath, AlbumCoverPath)
-VALUES ('Grupo Frontera x Ke Personajes - OJITOS ROJOS', (SELECT Id FROM Artist WHERE Name = 'Grupo Frontera'), '00:03:52', (SELECT Id FROM Album WHERE Name = 'El Comienzo'), '/ruta/ojitos_rojos.mp3', '/ruta/el_comienzo.jpg'); 
-INSERT INTO Song (Title, ArtistId, Duration, AlbumId, FilePath, AlbumCoverPath)
-VALUES ('Nuestra Cancion', (SELECT Id FROM Artist WHERE Name = 'Elvis Crespo'), '00:03:29', (SELECT Id FROM Album WHERE Name = 'Suavemente'), '/ruta/nuestra_cancion.mp3', '/ruta/suavemente.jpg'); 
-INSERT INTO Song (Title, ArtistId, Duration, AlbumId, FilePath, AlbumCoverPath)
-VALUES ('Don''t Cry', (SELECT Id FROM Artist WHERE Name = 'Guns N''Roses'), '00:04:44', (SELECT Id FROM Album WHERE Name = 'Use Your Illusion I'), '/ruta/dont_cry.mp3', '/ruta/use_your_illusion_i.jpg'); 
-INSERT INTO Song (Title, ArtistId, Duration, AlbumId, FilePath, AlbumCoverPath)
-VALUES ('No Digas Na', (SELECT Id FROM Artist WHERE Name = 'Feid'), '00:03:29', (SELECT Id FROM Album WHERE Name = 'MANIFESTTING 20-05'), '/ruta/no_digas_na.mp3', '/ruta/manifesting_2005.jpg');
-
